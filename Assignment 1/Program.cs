4d1134149c57ba89fd71b2e6ca0e5f31cb5bac5c
shivamm-mehta <mehtas25@mymacewan.ca>
2024-03-09 19:20:05 -0700

    Assignment Submitted
    The program defines a `Student` class with an associated enum `StudentType`. It includes methods to create a student object with formatted IDs and output student information to the console, showcasing effective use of null-coalescing operators and format specifiers.

---
+++ Assignment 1/Program.cs
@@ -0,0 +1,17 @@
+﻿using Assignment_1;
+
+class Program
+{
+    static void Main()
+    {
+        var student1 = StudentMethods.CreateStudent(1, StudentType.FullTime, "Shivam", "Mehta");
+        var student2 = StudentMethods.CreateStudent(2, StudentType.FullTime, "Jhanvi", "Shah");
+        var student3 = StudentMethods.CreateStudent(3, StudentType.PartTime, "Kirtan", "Vyas");
+
+        Console.WriteLine("Assignment One");
+
+        StudentMethods.OutputStudent(student1);
+        StudentMethods.OutputStudent(student2);
+        StudentMethods.OutputStudent(student3);
+    }
+}
